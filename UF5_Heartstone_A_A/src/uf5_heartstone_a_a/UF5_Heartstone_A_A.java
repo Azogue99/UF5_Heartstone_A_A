4bfcad533d65e3d98918d7bdd1909c8ea03c7583
azogue99 <arna1af@gmail.com>
2024-04-09 11:44:42 +0200

    PrimeraVersion

--- UF5_Heartstone_A_A/src/uf5_heartstone_a_a/UF5_Heartstone_A_A.java
+++ UF5_Heartstone_A_A/src/uf5_heartstone_a_a/UF5_Heartstone_A_A.java
@@ -1,20 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package uf5_heartstone_a_a;
-
-/**
- *
- * @author arnau.azogue
- */
 public class UF5_Heartstone_A_A {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
 }
